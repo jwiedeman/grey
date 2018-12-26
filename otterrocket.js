8e731fc2e055baea64546f35c8b23882709e5be2
Joshua Wiedeman <vielodesign@gmail.com>
2018-12-26 09:50:57 -0800

    a
    a

--- otterrocket.js
+++ otterrocket.js
@@ -7,9 +7,22 @@
 //vanilla js getting a text value
 document.querySelector('#product-info-table').innerText
 
+// if element ID exists
+let myEle = document.getElementById("myElement");
+    if(myEle){
+        console.log(myEle)
+    }
 
+// Strip variable > Currency to int
+var currency = "-$4,400.50";
+var number = Number(currency.replace(/[^0-9.-]+/g,""));
 
-
+// if exists strip to number 
+var __a = document.getElementById('pitSalePriceBx').innerHTML;
+    if(__a){
+		let __b = Number(__a.replace(/[^0-9.-]+/g,""));
+       console.log(__b)
+    }
 
 
 /* VIEWCONTENT */
