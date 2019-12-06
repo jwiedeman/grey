3b2e63d8ea611d885df4363a3ed2465c2fc70373
Joshua Wiedeman <vielodesign@gmail.com>
2019-12-06 12:52:13 -0800

    flags

--- CTF/OTW/bandit.js
+++ CTF/OTW/bandit.js
@@ -1,64 +1,64 @@
 0
-u
-p
-f
+u bandit0
+p bandit0
+f boJ9jbbUNNfktd78OOpsqOltutMc3MY1
 
 
 1
-u
-p
-f
+u bandit1
+p boJ9jbbUNNfktd78OOpsqOltutMc3MY1
+f CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
 
 
 2
-u
-p
-f
+u bandit2
+p CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
+f UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
 
 
 3
-u
-p
-f
+u bandit3
+p UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
+f pIwrPrtPN36QITSp3EQaw936yaFoFgAB
 
 
 4
-u
+u bandit4
 p
-f
+f koReBOKuIDDepwhWk7jZC0RTdopnAYKh
 
 
 5
-u
-p
-f
+u bandit5 
+p koReBOKuIDDepwhWk7jZC0RTdopnAYKh
+f DXjZPULLxYr17uwoI01bNLQbtFemEgo7
 
 
 6
-u
-p
-f
+u bandit6
+p DXjZPULLxYr17uwoI01bNLQbtFemEgo7
+f 
 
 
 7
-u
-p
-f
+u bandit7
+p DXjZPULLxYr17uwoI01bNLQbtFemEgo7
+f HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs
 
 
 8
-u
-p
-f
+u bandit8
+p cvX2JJa4CFALtqS87jk27qwqGhBM9plV
+f UsvVyFSfZZWbi6wgC7dAFyFuR6jQQUhR
 
 
 9
-u
-p
-f
+u bandit9
+p UsvVyFSfZZWbi6wgC7dAFyFuR6jQQUhR
+f truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk
 
 
 10
-u
-p
+u bandit10
+p truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk
 f
