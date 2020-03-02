4dbf3ca07ecf778fe2369ff71ff356d8f38eb4b3
Joshua Wiedeman <vielodesign@gmail.com>
2020-03-01 16:25:54 -0800

    adding haversine

--- otterrocket.js
+++ otterrocket.js
@@ -13,6 +13,16 @@ function getAge(DOB) {
 
 alert(getAge("1983-08-15T20:04:17.281Z"));
 
+function haversine() {
+       var radians = Array.prototype.map.call(arguments, function(deg) { return deg/180.0 * Math.PI; });
+       var lat1 = radians[0], lon1 = radians[1], lat2 = radians[2], lon2 = radians[3];
+       var R = 6372.8; // km
+       var dLat = lat2 - lat1;
+       var dLon = lon2 - lon1;
+       var a = Math.sin(dLat / 2) * Math.sin(dLat /2) + Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
+       var c = 2 * Math.asin(Math.sqrt(a));
+       return R * c;
+}
 
 
 js preformance check 
